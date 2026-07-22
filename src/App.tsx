fc9aea13e90c2598bca6de90b45fe381e9ebdc1d
Renato Cruz <at.renatocruz@gmail.com>
2026-07-22 12:35:11 +0100

    Compose full page layout in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,33 @@
+import { content } from './content'
+import { Eyebrow } from './components/Eyebrow'
+import { Headline } from './components/Headline'
+import { BodyText } from './components/BodyText'
+import { HeroImage } from './components/HeroImage'
+import { CtaButton } from './components/CtaButton'
+import { SocialIcons } from './components/SocialIcons'
+
 function App() {
-  return <div>placeholder</div>
+  return (
+    <div className="page">
+      <div className="card">
+        <HeroImage src={content.heroImageUrl} alt={content.heroImageAlt} />
+        <div className="card-content">
+          <Eyebrow text={content.eyebrow} />
+          <Headline text={content.headline} />
+          <BodyText text={content.body} />
+          <div className="cta-group">
+            {content.ctas.map((cta) => (
+              <CtaButton key={cta.label} {...cta} />
+            ))}
+          </div>
+          <SocialIcons
+            emailHref={content.social.emailHref}
+            instagramHref={content.social.instagramHref}
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default App
